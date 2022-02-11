895064ba24c7a102ee33678c70356d8a66a20feb
Fer Sánchez <sangarfer@gmail.com>
2022-02-11 15:36:37 +0100

    last changes done

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -5,33 +5,6 @@ const express = require('express');
 const BookModel = require('../models/Book.model');
 const router = express.Router();
 
-// ****************************************************************************************
-// GET route to display all the books
-// ****************************************************************************************
-
-// this corresponds to the app.get()
-router.get("/books", (req, res)=>{
-
-  BookModel.find()
-  .then(books=>{
-    console.log(`Found ${books.length} books from the DB`);
-    res.render("books-list", {books});
-  })
-})
-
-// ****************************************************************************************
-// GET route for querying a specific book from the database
-// ****************************************************************************************
-
-router.get("/books/:idFromTheDB", (req, res)=>{
-  const id = req.params.idFromTheDB;
-  BookModel.findById(id)
-  .then(book=>{
-    console.log(`Read this book from the DB: ${book}`)
-    res.render("book-details", book)
-  })
-  .catch(()=> console.log("DB error reading '/books'"))
-})
 
 // ****************************************************************************************
 // GET route for updating a specific book from the database
@@ -60,5 +33,56 @@ router.route("/books/:idFromTheDB/edit")
     res.redirect(`/books/${id}`);
   })
 })
+
+// ****************************************************************************************
+// GET route to create all the books
+// ****************************************************************************************
+
+router.route("/books/create")
+.get((req, res) => {
+  res.render("book-create");
+})
+.post((req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const author = req.body.author;
+  const rating = req.body.rating;
+
+  BookModel.create({ title, author, description, rating })
+  // .then(bookFromDB => console.log(`New book created: ${bookFromDB.title}.`))
+  .then(() => res.redirect("/books"))
+  .catch((error) => `Error while creating a new book: ${error}`);
+});
+
+
+// ****************************************************************************************
+// GET route for querying a specific book from the database
+// ****************************************************************************************
+
+router.get("/books/:idFromTheDB", (req, res)=>{
+  const id = req.params.idFromTheDB;
+  BookModel.findById(id)
+  .then(book=>{
+    console.log(`Read this book from the DB: ${book}`)
+    res.render("book-details", book)
+  })
+  .catch(()=> console.log("DB error reading '/books'"))
+})
+
+
+// ****************************************************************************************
+// GET route to display all the books
+// ****************************************************************************************
+
+// this corresponds to the app.get()
+router.get("/books", (req, res)=>{
+
+  BookModel.find()
+  .then(books=>{
+    console.log(`Found ${books.length} books from the DB`);
+    res.render("books-list", {books});
+  })
+})
+
 
 module.exports = router;
