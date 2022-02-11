ff8635b27ae3fa5ed76df22224421f03d77ed0b2
Fer Sánchez <sangarfer@gmail.com>
2022-02-11 13:51:38 +0100

    lunch break

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require("dotenv/config")
 
 mongoose
-  .connect('mongodb://localhost/library-project')
+  .connect(`mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PASSWORD}@cluster0.5zqfb.mongodb.net/library-project?authSource=admin&replicaSet=atlas-ffdmfo-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`)
   .then(x =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
